0b763afbeb0770ebb22f0875b3473ad0d9614058
Liam <ljackson61@my.bcit.ca>
2024-12-04 14:33:07 -0800

    redirect to /portfolio

--- ws_project2/urls.py
+++ ws_project2/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/portfolio/', permanent=True)),
     path('admin/', admin.site.urls),
     path('portfolio/', include('portfolio.urls')),
     path('accounts/', include('allauth.urls')),
