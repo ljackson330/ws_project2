3cee20c79e74d220fe4a56b554287dc281313a12
Liam <ljackson61@my.bcit.ca>
2024-12-04 16:23:41 -0800

    redirect

--- ws_project2/urls.py
+++ ws_project2/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/portfolio/', permanent=False)),
     path('admin/', admin.site.urls),
     path('portfolio/', include('portfolio.urls')),
     path('accounts/', include('allauth.urls')),
